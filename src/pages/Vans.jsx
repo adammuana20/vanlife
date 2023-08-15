47b3368ef12ea09fb0a6569c4a12046b9e5b31bd
Adam Clint Muana <Adam.Muana@entaingroup.com>
2023-08-15 18:01:26 +0800

    fetch vans

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default function Vans() {
+    const [vans, setVans] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data))
+    }, [])
+
     return (
         <h1>Vans Page goes here</h1>
     )
