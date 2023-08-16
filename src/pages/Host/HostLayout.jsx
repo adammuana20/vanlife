0a0c83e9a068c02c27f2b216a396f2d985e00c3b
Adam Clint Muana <Adam.Muana@entaingroup.com>
2023-08-17 05:48:38 +0800

    added hostVans page and make the content

--- src/pages/Host/HostLayout.jsx
+++ src/pages/Host/HostLayout.jsx
@@ -8,12 +8,34 @@ export default function HostLayout() {
         color: "#161616"
     }
 
+
     return (
         <>
             <nav className="host-nav">
-                <NavLink to="/host" end style={({isActive}) => isActive ? activeStyle : null}>Dashboard</NavLink>
-                <NavLink to="/host/income" style={({isActive}) => isActive ? activeStyle : null}>Income</NavLink>
-                <NavLink to="/host/reviews" style={({isActive}) => isActive ? activeStyle : null}>Reviews</NavLink>
+                <NavLink to="/host" 
+                    end 
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    Dashboard
+                </NavLink>
+                <NavLink 
+                    to="/host/income" 
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    Income
+                </NavLink>
+                <NavLink 
+                    to="/host/reviews" 
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    Reviews
+                </NavLink>
+                <NavLink 
+                    to="/host/vans" 
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    Vans
+                </NavLink>
             </nav>
             <Outlet />
         </>
---
+++ src/pages/Host/HostVanDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function VanDetail() {
+    return (
+        <h1>Van Details page</h1>
+    )
+}
