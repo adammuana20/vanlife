0708c164dca0d8fe933465f1e35c9635a66308fc
Adam Clint Muana <Adam.Muana@entaingroup.com>
2024-05-10 01:42:06 +0800

    Added load more functionality on host vans

--- src/components/Host/HostVansPreview.tsx
+++ src/components/Host/HostVansPreview.tsx
@@ -1,17 +1,41 @@
 import { useAsyncValue } from 'react-router-dom'
+import { useState } from 'react'
 
 import HostVanCard from './HostVanCard'
 import NoState from '../NoState'
 
 import { Van } from '../../utils/firebase'
+import Button from '../Button'
 
 const HostVansPreview = () => {
     const vans = useAsyncValue() as Van[]
+    const [limit, setLimit] = useState<number>(4)
+
+    const loadMore = () => {
+        setLimit((prev: number) => prev + 2)
+    }
+
   return (
     vans.length ? (
         <>
             <h2>Your listed vans</h2>
-            {vans.map((van) => <HostVanCard van={van} key={van.id} />)}
+            {vans
+                .filter((_, idx) => idx < limit)
+                .map((van) => <HostVanCard van={van} key={van.id} />)
+            }
+            { vans.length <= limit
+                ? null
+                : (
+                    <div className='flex justify-center items-center'>
+                        <div className='w-1/6'>
+                            <Button
+                                label='Load More'
+                                onClick={loadMore}
+                            />
+                        </div>
+                    </div>
+                )
+            }
         </>
     ) : (
         <NoState 
